7b9056973b502bfe537df521e0fdfc1cf0beaac6
Jcwscience <jcwscience@me.com>
2024-03-28 23:42:49 -0600

    Modularization attempt

---
+++ cli.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import argparse  # Import the argparse library
+import sounddevice as sd
+import os
+from xtts_live.main import TextToSpeechGenerator
+
+# Set up the argument parser
+parser = argparse.ArgumentParser(description="TTS Generator with Command Line Arguments")
+parser.add_argument("--model_path", type=str, default="./XTTS-v2/", help="Path to the TTS model directory")
+parser.add_argument("--speaker_wavs", type=str, help="List of speaker WAV files for conditioning")
+parser.add_argument("--language", type=str, default="en", help="Language for the TTS model")
+parser.add_argument("--output_device", type=int, help="Output device ID for sound playback")
+parser.add_argument("--model_temperature", type=float, default=0.65, help="Temperature parameter for the TTS model")
+parser.add_argument("--list_devices", action="store_true", help="List available audio devices")
+parser.add_argument("--use_deepspeed", type=bool, default=False, help="Use DeepSpeed for faster inference")
+parser.add_argument("--cpu_only", type=bool, default=False, help="Use CPU for inference")
+parser.add_argument("--enable_text_splitting", type=bool, default=True, help="Use text splitting for long texts")
+parser.add_argument("--output_samplerate", type=int, default=48000, help="Output samplerate for the audio stream")
+
+
+# Parse the arguments
+args = parser.parse_args()
+
+# Use the parsed arguments
+if args.list_devices:
+    print(sd.query_devices())
+    exit(0)
+
+model_path = args.model_path
+if not os.path.exists(model_path):
+    print("Error: Please provide a valid model path with --model_path.")
+    exit(1)
+
+speaker_wavs = args.speaker_wavs
+output_device = args.output_device
+output_samplerate = args.output_samplerate
+enable_text_splitting = args.enable_text_splitting
+use_deepspeed = args.use_deepspeed
+language = args.language
+cpu_only = args.cpu_only
+model_temperature = args.model_temperature
+
+# Initialize the synthesizer
+tts = TextToSpeechGenerator(model_path, speaker_wavs, output_device, use_deepspeed, cpu_only, output_samplerate)
+
+tts.start()
+
+try:
+    while True:
+        input_text = input("Enter text: ")
+        tts.speak(input_text, language, model_temperature, enable_text_splitting)
+
+except KeyboardInterrupt:
+    tts.stop()
---
+++ xtts_live/main.py
@@ -0,0 +1,101 @@
+import numpy as np
+import threading
+import librosa
+import sounddevice as sd
+from TTS.tts.configs.xtts_config import XttsConfig
+from TTS.tts.models.xtts import Xtts
+import queue
+
+class AudioBuffer:
+    def __init__(self):
+        self.buffer = np.array([], dtype=np.float32)
+        self.keep_stream_open = True  # Flag to determine behavior when buffer is empty
+
+    def add_data(self, new_data):
+        # Append new data to the buffer.
+        self.buffer = np.concatenate((self.buffer, new_data), axis=0)
+
+    def get_samples(self, n_samples):
+        if len(self.buffer) > 0:
+            if n_samples <= len(self.buffer):
+                # Enough data available, return requested samples
+                samples = self.buffer[:n_samples].reshape(-1, 1)
+                self.buffer = self.buffer[n_samples:]
+            else:
+                # Not enough data, pad with zeros
+                padding = np.zeros(n_samples - len(self.buffer), dtype=np.float32)
+                samples = np.concatenate((self.buffer, padding), axis=0).reshape(-1, 1)
+                self.buffer = np.array([], dtype=np.float32)  # Clear the buffer
+            return samples
+        elif self.keep_stream_open:
+            # Buffer is empty, but we want to keep the stream open
+            return np.zeros(n_samples, dtype=np.float32).reshape(-1, 1)
+        else:
+            # Buffer is empty and we want to close the stream
+            return None
+
+class TextToSpeechGenerator:
+    def __init__(self, model_path, speaker_wavs, output_device, use_deepspeed=False, cpu_only=False, output_samplerate=48000, **kwargs):
+        self.output_device = output_device
+        self.samplerate = output_samplerate
+        self.kwargs = kwargs
+        self.task_queue = queue.Queue()
+        self.audio_buffer = AudioBuffer()
+        self.running = False
+        self.thread = threading.Thread(target=self._process_queue)
+        config = XttsConfig()
+        config.load_json(model_path + "config.json")
+        model = Xtts.init_from_config(config)
+        model.load_checkpoint(config, checkpoint_dir=model_path, use_deepspeed=use_deepspeed)
+        if cpu_only:
+            self.model = model
+        else:
+            self.model = model.cuda()
+        self.gpt_cond_latent, self.speaker_embedding = model.get_conditioning_latents(audio_path=speaker_wavs)
+
+
+    def _process_queue(self):
+        while self.running:
+            task = self.task_queue.get()
+            self._speak(task['text'], task['language'], task['gpt_cond_latent'], task['speaker_embedding'], task["speaker_wavs"], task['temperature'], task['enable_text_splitting'])
+
+    def _stream_callback(self, outdata, frames, time, status):
+        # Get samples from the buffer
+        data = self.audio_buffer.get_samples(frames)
+        if data is not None:
+            outdata[:] = data
+        else:
+            # End stream
+            sd.CallbackAbort
+
+    def _speak(self, text, language, gpt_cond_latent, speaker_embedding, speaker_wavs, temperature, enable_text_splitting):
+        chunks = self.model.inference_stream(text, language, gpt_cond_latent, speaker_embedding, temperature, enable_text_splitting)
+        self.audio_buffer.keep_stream_open = True
+        for chunk in chunks:
+            # If the chunk is a tensor, convert it to a NumPy array and ensure it's a float32 array
+            if hasattr(chunk, 'cpu'):
+                chunk = chunk.cpu().numpy()
+            chunk = chunk.astype(np.float32)
+            chunk = librosa.resample(chunk, orig_sr=24000, target_sr=self.samplerate)
+            self.audio_buffer.add_data(chunk)
+        self.audio_buffer.keep_stream_open = False
+
+    def start(self):
+        self.stream = sd.OutputStream(samplerate=self.samplerate, device=self.output_device, channels=1, dtype=np.float32, blocksize=2048, callback=self._stream_callback)
+        self.stream.start()
+        self.running = True
+        self.thread.start()
+        print("Audio stream started.")
+
+    def speak(self, text, language="en", speaker_wavs=None, temperature=0.65, enable_text_splitting=True):
+        self.task_queue.put({'text': text, 'language': language, 'gpt_cond_latent': self.gpt_cond_latent, 'speaker_embedding': self.speaker_embedding, 'speaker_wavs': speaker_wavs, 'temperature': temperature, 'enable_text_splitting': enable_text_splitting})
+
+    def stop(self):
+        self.running = False
+        self.task_queue.put(None)
+        self.stream.close()
+        print("Stopping audio stream...")
+        self.thread.join()
+        print("Audio stream stopped.")
+        
+        
